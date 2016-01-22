4e1f39a0acf805084ece7f243f03fcb73202bbc7
Rob Rothe <robrothe@gmail.com>
2016-01-22 15:32:04 -0600

    Added delete option

--- app.js
+++ app.js
@@ -42,10 +42,16 @@
 		var vm = this;
 		vm.musicians = Data.musicians;
 		vm.edit = edit;
+		vm.delete = deleteM;
 
 		function edit(musician) {
 			var form_data = Data.copy(musician);
 			$rootScope.$broadcast('new_form_data',form_data);
+		}
+
+		function deleteM(musician) {
+			var idx = Data.musicians.indexOf(musician);
+			Data.musicians.splice(idx,1);
 		}
 	}
 
